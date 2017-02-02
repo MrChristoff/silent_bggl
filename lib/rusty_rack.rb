6594d35c8f1bfba8b1247ef6edca82242085c876
Kath Pobee-Norris <kathwath@hotmail.com>
2017-02-02 12:24:50 +0000

    Added ability to inspect params

--- lib/rusty_rack.rb
+++ lib/rusty_rack.rb
@@ -18,10 +18,15 @@ module RustyRack
       requested_path = @request.path_info
       handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
       if handler
-        handler.call
+        # handler.call
+        instance_eval(&handler) #instance_eval gives access to all methods and instance variables in RustyRack::Base
       else
         [404, {}, ["Arrr, no treasure here matey.\n\nAfraid '#{verb} #{requested_path}' does not exist."]]
       end
+    end
+
+    def params
+      @request.params
     end
 
     private
@@ -36,6 +41,10 @@ rusty_rack = RustyRack::Base.new
 
 rusty_rack.get "/hello" do #creates a 'GET' route
   [200, {}, ["Welcome to the rusty rack, arrr"]] #creates the respose to the request on this route
+end
+
+rusty_rack.get "/" do
+  [200, {}, ["Your params arrrrrr #{params.inspect}"]]
 end
 
 Rack::Handler::WEBrick.run rusty_rack, Port: 9292
